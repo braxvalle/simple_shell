0c4d56841728729ed64fac9bd02bd5bd3afd2a26
eunice603 <euniceorina254@gmail.com>
2022-05-15 05:55:57 -0700

    Betty checks

---
+++ betty.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+/* printf() displays the string inside quotation*/
+
+int main(void)
+{
+printf("Hello, World!");
+return (0);
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --                                                                                                        0,1           All
+
